edc18e6dd8d76cff16c83b9de97e3e66b6f03ee5
TomatoGremlin <101596047+TomatoGremlin@users.noreply.github.com>
2022-07-25 14:57:54 +0300

    Update test1.c

--- test1.c
+++ test1.c
@@ -1,36 +1,29 @@
 #include <stdio.h>
 #include <stdarg.h> 
-#include <float.h>
-#include <stdint.h>
 
-int checkBit(size_t *mask, size_t bit){
-    
-    int index = bit / (sizeof(*mask) * __CHAR_BIT__);;
-    int b = bit % (sizeof(*mask) * __CHAR_BIT__);
-    return !!(mask[index] & (1ull << b)); 
-}
-
-size_t bitsNCount (size_t maskcount, size_t bitcount, ...){
-    size_t masks = 0;
+size_t onesCount (size_t count, ...){
+    size_t ones = 0;
     va_list args;
-    va_start(args,maskcount);
+    va_start(args,count);
 
-    for (int i = 0; i < maskcount; i++){
+    for (int i = 0; i < count; i++){
         int mask = va_arg(args, int);
-        int maskBists = 0;
-        for (size_t i = 0; i < sizeof(mask)*8; i++){
-            maskBists += checkBit(mask, i);
+        for (size_t j = 0; j < sizeof(mask)*8; j++){
+            if(mask & (1ull << j)){
+                ones ++ ;
+            }
         }
-        masks += (maskBists == bitcount);
-  
     }
     va_end(args);
-    
-    return masks;
+    printf("%d\n", ones );
+    return ones;
 }
 
 int main() {
-    
-
+    onesCount (4, 0x0a, 0, 1, 2);
+    onesCount (3, 0xff, 0x131, 0xaaaa);
+    onesCount (2, 0b0001, 9);
     return 0;
 }
+
+
