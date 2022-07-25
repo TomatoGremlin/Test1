ada70ade403853e87afc7ffad305c6d871aa6765
TomatoGremlin <101596047+TomatoGremlin@users.noreply.github.com>
2022-07-25 14:58:14 +0300

    Update test2.c

--- test2.c
+++ test2.c
@@ -1,46 +1,49 @@
+
 #include <stdio.h>
 #include <stdint.h>
-#define INT_SIZE sizeof(int) * 8
 
-void  flopOddBits(uint64_t mask)
-{
-    for(int i=0; i<32; i+=2){
-        int i_bit = ( mask >> i ) & 1; 
-        int i_1_bit = (mask >> ( i+1 )) & 1;  
-         
-        mask = mask - ( i_bit << i) - ( i_1_bit << ( i+1 ) ) + ( i_bit << ( i+1 ) ) + ( i_1_bit << i );  
-
+unsigned zerosCount(uint64_t mask1, uint32_t mask2, uint16_t mask3, uint8_t mask4){
+    unsigned count = 0;
+    for (int bit = 0; bit < sizeof(uint64_t) * 8; bit++){
+        if ((mask1 & (1UL << bit)) >> bit == 0)
+        {
+            count ++ ;
+        }
+        
     }
+    for (int bit = 0; bit < sizeof(uint32_t) * 8; bit++){
+        if ((mask2 & (1UL << bit)) >> bit == 0)
+        {
+            count ++ ;
+        }
+    }
+    for (int bit = 0; bit < sizeof(uint16_t) * 8; bit++){
+        if ((mask3 & (1UL << bit)) >> bit == 0)
+        {
+            count ++ ;
+        }
+    }
+    for (int bit = 0; bit < sizeof(uint8_t) * 8; bit++){
+        if ((mask4 & (1UL << bit)) >> bit == 0)
+        {
+            count ++ ;
+        }
+    }
+    return count;
+}
+
+void flipOddBits(uint64_t* mask){
+    for (int bit = 1; bit < sizeof(uint64_t) * 8; bit += 2){
+       *mask ^= (1UL << bit);
+    }
+}
+
+int main() {
+   printf("Ones count: %u\n", zerosCount(0, 0, 0, 0));
+   uint64_t mask = 0;
+   flipOddBits(&mask);
+ 
+    return 0;
+}
    
-}
- 
-void zerosCount()
-{
-    
-    int num, zeros, ones, i;
 
-    printf("Enter any number: ");
-    scanf("%d", &num);
-
-    zeros = 0;
-
-    for(i=0; i<INT_SIZE; i++)
-    {   
-        if(!(num & 1))
-            zeros++;
-
-        num >>= 1;
-    }
-
-    printf("Total zero bit is %d\n", zeros);
-
-    return 0;
-}
- 
-
-int main()
-{
-    
- 
-    return 0;
-}
